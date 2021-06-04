e59c9d2751d6ca282094e533f577e8f1452fe104
m26416141 <m26416141@john.petra.ac.id>
2021-06-04 10:56:39 +0800

    updated

--- contactus.php
+++ contactus.php
@@ -25,9 +25,7 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="index.html">Who We Are
-						<!-- <span class="sr-only">(current)</span> -->
-					</a>
+					<a class="nav-link" href="index">Who We Are</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="services.html">Services</a>
