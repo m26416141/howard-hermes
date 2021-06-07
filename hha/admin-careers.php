67fec7db8b81b7c249d2aacb3f01d2357c3f5c8c
m26416141 <m26416141@john.petra.ac.id>
2021-06-07 23:22:01 +0800

    updated admin page

---
+++ hha/admin-careers.php
@@ -0,0 +1,1 @@
+<h1>careers page</h1>
--- hha/admin.php
+++ hha/admin.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height: 100%;">
 
 <head>
     <meta charset="UTF-8">
@@ -9,43 +9,77 @@
     <title>Admin - Howard Hermes</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../public/css/admin.css" />
 </head>
 
-<body>
-    <div class="sidebar">
-        <div class="row logo-content">
-            <div class="col-sm-10 logo">
-                <p class="logo-name" style="font-weight: 700;">Howard Hermes</p>
+<body style="height: 100%;">
+    <div class="container-fluid" style="height: 100%;">
+        <div class="row" style="height: 100%;">
+            <div class="w-25 sidebar" id="sidebar" style="max-height: 100%;">
+                <div class="row logo-content">
+                    <div class="col-sm-10 logo">
+                        <p class="logo-name" style="font-weight: 700;">Howard Hermes</p>
+                    </div>
+                    <div class="col-sm-2">
+                        <i class='bx bx-menu'></i>
+                    </div>
+                </div>
+                <div class="logo-border"></div>
+                <ul class="list-group">
+                    <li class="list-group-item" id="messages">
+                        <a id="text-messages" href="#" data-target="admin-messages" onclick="loadMessages()">
+                            <!-- <box-icon name='chat' type='solid'></box-icon> -->
+                            <i class='bx bxs-message-error'></i>
+                            <span class="links-name">Messages</span>
+                        </a>
+                    </li>
+                    <li class="list-group-item" id="careers">
+                        <a id="text-careers" href="#" data-target="admin-careers" onclick="loadCareers()">
+                            <!-- <box-icon type='solid' name='group'></box-icon> -->
+                            <i class='bx bxs-group'></i>
+                            <span class="links-name">Careers</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <div class="col-sm-2">
-                <box-icon name='menu' class="menu-logo"></box-icon>
+            <div class="content" id="content">
             </div>
         </div>
-        <div class="logo-border"></div>
-        <ul class="list-group">
-            <li class="list-group-item">
-                <a href="">
-                    <box-icon type='solid' name='home'></box-icon>
-                    <span class="links-name">Home</span>
-                </a>
-            </li>
-            <li class="list-group-item">
-                <a href="">
-                    <box-icon name='chat' type='solid'></box-icon>
-                    <span class="links-name">Contact</span>
-                </a>
-            </li>
-            <li class="list-group-item">
-                <a href="">
-                    <box-icon type='solid' name='group'></box-icon>
-                    <span class="links-name">Careers</span>
-                </a>
-            </li>
-        </ul>
     </div>
-    <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
+    <!-- <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script> -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        function loadMessages() {
+            document.getElementById("messages").style.backgroundColor = "#793775";
+            document.getElementById("text-messages").style.color = "white";
+            document.getElementById("careers").style.backgroundColor = "#f2f2f2";
+            document.getElementById("text-careers").style.color = "black";
+        }
+
+        function loadCareers() {
+            document.getElementById("careers").style.backgroundColor = "#793775";
+            document.getElementById("text-careers").style.color = "white";
+            document.getElementById("messages").style.backgroundColor = "#f2f2f2";
+            document.getElementById("text-messages").style.color = "black";
+        }
+    </script>
+    <script>
+        $(document).ready(function() {
+            var trigger = $('#sidebar ul li a'),
+                container = $('#content');
+
+            trigger.on('click', function() {
+                var $this = $(this),
+                    target = $(this).data('target');
+
+                $("#content").load(target + '.php');
+
+                return false;
+            });
+        });
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
