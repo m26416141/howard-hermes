8ce54c4dab3dc839294e8d24181297956cd9ec37
m26416141 <m26416141@john.petra.ac.id>
2021-06-07 16:35:25 +0800

    added update to admin page

--- hha/admin.php
+++ hha/admin.php
@@ -4,12 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Admin - Howard Hermes</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="stylesheet" href="../public/css/admin.css" />
 </head>
 
 <body>
-
+    <div class="sidebar">
+        <div class="row logo-content">
+            <div class="col-sm-10 logo">
+                <p class="logo-name" style="font-weight: 700;">Howard Hermes</p>
+            </div>
+            <div class="col-sm-2">
+                <box-icon name='menu' class="menu-logo"></box-icon>
+            </div>
+        </div>
+        <div class="logo-border"></div>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="">
+                    <box-icon type='solid' name='home'></box-icon>
+                    <span class="links-name">Home</span>
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="">
+                    <box-icon name='chat' type='solid'></box-icon>
+                    <span class="links-name">Contact</span>
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="">
+                    <box-icon type='solid' name='group'></box-icon>
+                    <span class="links-name">Careers</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
